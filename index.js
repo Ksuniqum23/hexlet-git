a3b993562d753b311a04420b2a4939da89f7b26c
Kseniya Freeflyer <kalyakina.ks.a@gmail.com>
2024-05-19 18:20:01 +0300

    add lodash

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-//
+import _ from "lodash";
+// import * as _123 from "lodash";
+// import { last } from "lodash";
 console.log('Hello, Hexlet!');
 console.log('Hello, Ivan!');
+console.log(_.last(['one', 'two']));
